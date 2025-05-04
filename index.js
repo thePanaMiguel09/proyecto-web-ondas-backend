71c82fcd1cdad237bc192e439eef27c2163c4d32
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-05-04 16:00:33 -0500

    CORS added

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-require("dotenv").config();
+const cors = require("cors")
+
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const express = require("express");
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}))
 
 const PORT = 3000;
 
 const connectDB = require("./config/db");
 connectDB();
 
-
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-  res.send("Hello World from Miguel Angel Chavez");
+  res.json({"name":"Miguel",
+    "lastName": "Chavez"
+  })
 });
 
 app.listen(PORT, () => {
