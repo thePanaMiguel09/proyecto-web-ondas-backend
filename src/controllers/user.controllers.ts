2654775c8517bcf36cae839e3c6fd390d146d035
thePanaMiguel09 <miguichavezbarreram@gmail.com>
2025-05-16 00:58:25 -0500

    req body modified, creation of SUPERADMIN

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,18 +1,16 @@
 import { Request, RequestHandler, Response } from "express";
+import bcrypt from "bcryptjs";
 import Usuario from "../models/User";
 import { Estudiante } from "../models/Estudiante";
 import { Docente } from "../models/Docente";
 
+//Completar el registro de datos
 export const registrarUsuario = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   const {
     id,
-    primerNombre,
-    segundoNombre,
-    primerApellido,
-    segundoApellido,
     tipoIdentificacion,
     numeroIdentificacion,
     fechaNacimiento,
@@ -24,6 +22,7 @@ export const registrarUsuario = async (
     gradoEscolar,
     materiaAsignada,
   } = req.body;
+
 
   try {
     const findUser = await Usuario.findById(id);
@@ -39,10 +38,6 @@ export const registrarUsuario = async (
     }
 
     findUser.set({
-      primerNombre,
-      segundoNombre,
-      primerApellido,
-      segundoApellido,
       tipoIdentificacion,
       numeroIdentificacion,
       fechaNacimiento,
@@ -73,11 +68,72 @@ export const registrarUsuario = async (
   }
 };
 
+//Obtener todos los usuarios
 export const getUsers: RequestHandler = async (req: Request, res: Response) => {
   try {
     const data = await Usuario.find();
     res.status(200).json({ msg: "Usuarios obtenidos con éxtio", data: data });
   } catch (error) {
     res.status(500).json({ msg: "Error al obtener usuarios" });
+  }
+};
+
+
+
+//Crear súper admin
+export const createAdmin: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { email, password, firstName, firstLastName, seconLastName } = req.body;
+
+  const findedEmail = await Usuario.findOne({ email: email });
+  if (findedEmail) {
+    res.status(401).json({ msg: "Este correo ya existe" });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUserAdmin = new Usuario({
+    email: email,
+    contraseña: hashedPassword,
+    primerNombre: firstName,
+    primerApellido: firstLastName,
+    segundoApellido: seconLastName,
+    rol: "SUPERADMIN",
+  });
+
+  try {
+    await newUserAdmin.save();
+    res.status(201).json({ msg: "SuperAdmin creado" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+//Eliminacion lógica de un usuario, se modifica el estado, se busca el usuario por su docID
+export const deleteUser: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  try {
+    const user = await Usuario.findOneAndUpdate(
+      { numeroIdentificacion: Number(id) },
+      { isActive: false },
+      {
+        new: true,
+      }
+    );
+
+    if (!user) {
+      res.status(400).json({ msg: "No fue posible eliminar el usuario" });
+      return;
+    }
+
+    res.status(200).json({ msg: "Usuario eliminado" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error interno del servidor", err: error });
   }
 };
